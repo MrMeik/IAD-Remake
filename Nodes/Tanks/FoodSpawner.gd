a37e781c3cf5a6943008a50a1366ecadaac2e6bf
MrMeik <mlabarbera98a@gmail.com>
2024-01-30 20:39:11 -0500

    Added price to food pellets

--- Nodes/Tanks/FoodSpawner.gd
+++ Nodes/Tanks/FoodSpawner.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+@onready var _gui: GuiController = $"../GUI"
+
 @export var food_to_spawn: PackedScene
 @export var max_food_alive: int = 10
 
 # Should probably depend on what the food max amount is
 @export var placement_timeout: float = 0.25 #seconds
+
+const FOOD_PRICE = 5
 
 var current_timeout: float = 0
 
@@ -27,12 +31,14 @@ func _process(delta: float) -> void:
 			
 			# Add current_timeout which may be negatrive to next timeout to account for render timing differences
 			current_timeout = placement_timeout + current_timeout
+			
+			_gui.update_price(-FOOD_PRICE)
 	else:
 		# Reset the timeout when letting go of mouse (meaning can place as fast as mouse click)
 		current_timeout = 0
 
 func can_place_new_pellet() -> bool:
-	return current_timeout <= 0 and get_children().size() < max_food_alive 
+	return current_timeout <= 0 and get_children().size() < max_food_alive and _gui.can_buy(FOOD_PRICE)
 	
 func is_cursor_out_of_bounds(cursor_pos: Vector2) -> bool:
 	return cursor_pos.y > 119 or cursor_pos.y < -75 or cursor_pos.x < -225 or cursor_pos.x > 225
